22972119aca2ed0300055dffa7a7e5c7b9121384
Kevin <mrhallonline@gmail.com>
2019-11-10 21:21:43 -0500

    extracted Reusable Card

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -14,50 +14,53 @@ class _InputPageState extends State<InputPage> {
       ),
       body: Column(
         children: <Widget>[
-          Expanded(child: Row(
-            children: <Widget>[
-              Expanded(child: Container(
-        decoration: BoxDecoration(
-          color: Color(0xFF1D1E33),
-          borderRadius: BorderRadius.circular(10),
-        ),
-        margin: EdgeInsets.all(15.0),
-      ),),
-              Expanded(child: Container(
-        decoration: BoxDecoration(
-          color: Color(0xFF1D1E33),
-          borderRadius: BorderRadius.circular(10),
-        ),
-        margin: EdgeInsets.all(15.0),
-      ),),
-            ],
-          ),),
-          Expanded(child: Container(
-        decoration: BoxDecoration(
-          color: Color(0xFF1D1E33),
-          borderRadius: BorderRadius.circular(10),
-        ),
-        margin: EdgeInsets.all(15.0),
-      ),),
-          Expanded(child: Row(
-            children: <Widget>[
-              Expanded(child: Container(
-        decoration: BoxDecoration(
-          color: Color(0xFF1D1E33),
-          borderRadius: BorderRadius.circular(10),
-        ),
-        margin: EdgeInsets.all(15.0),
-      ),),
-              Expanded(child: Container(
-        decoration: BoxDecoration(
-          color: Color(0xFF1D1E33),
-          borderRadius: BorderRadius.circular(10),
-        ),
-        margin: EdgeInsets.all(15.0),
-      ),),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(colour: Color(0xFF1D1E33),),
+                ),
+                Expanded(
+                  child: ReusableCard(colour: Color(0xFF1D1E33),),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: new ReusableCard(colour: Color(0xFF1D1E33),),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(colour: Color(0xFF1D1E33),),
+                ),
+                Expanded(
+                  child: ReusableCard(colour: Color(0xFF1D1E33),),
+                ),
+              ],
+            ),
+          ),
         ],
-      ),),
-        ]),
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+
+  ReusableCard({@required this.colour});
+
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: EdgeInsets.all(15.0),
     );
   }
 }
